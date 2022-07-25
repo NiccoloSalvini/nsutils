957a09b6b9ea32fde5ed516b04e3dde7dcfcc289
Niccolò Salvini <niccolo.salvini27@gmail.com>
2022-07-25 13:44:10 +0200

    rename r file from `scale` to `ggutils`

---
+++ R/geojson.R
@@ -0,0 +1,11 @@
+#' @export
+#' @rdname provinces_gj_pointer
+provinces_gj_pointer = "https://raw.githubusercontent.com/openpolis/geojson-italy/master/geojson/limits_IT_provinces.geojson"
+
+#' @export
+#' @rdname regions_gj_pointer
+regions_gj_pointer = "https://raw.githubusercontent.com/openpolis/geojson-italy/master/geojson/limits_IT_regions.geojson"
+
+#' @export
+#' @rdname municipalities_gj_pointer
+municipalities_gj_pointer = "https://raw.githubusercontent.com/openpolis/geojson-italy/master/geojson/limits_IT_municipalities.geojson"
